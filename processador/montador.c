52263eba068518438ae1ffed85e9f6b84071deca
João Victor de Almeida <102709009+JvAlm71@users.noreply.github.com>
2023-12-20 14:22:33 -0300

    Update montador.c

--- processador/montador.c
+++ processador/montador.c
@@ -6,6 +6,8 @@ case XCHG_CODE:
 
 
 case XCHG_CODE:
+    parser_SkipUntil(','); 
+    parser_SkipUntilEnd(); 
     str_tmp1 = parser_GetItem_s();
     val1 = BuscaRegistrador(str_tmp1);
     free(str_tmp1);
@@ -17,7 +19,7 @@ case XCHG_CODE:
     str_tmp2 = ConverteRegistrador(val2);
     sprintf(str_msg, "%s%s%s0000", XCHG, str_tmp1, str_tmp2);
     parser_Write_Inst(str_msg, end_cnt);
-    end_cnt += 1;
+    end_cnt += 1; 
     free(str_tmp1);
     free(str_tmp2);
     break;
